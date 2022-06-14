79cd9e42e3ef9eb243f21d8561c9b950441e50bc
Marian Domanski <mmdski@gmail.com>
2022-06-14 13:32:11 -0500

    add implementations of matrix get, set, and new like

--- include/matrix.h
+++ include/matrix.h
@@ -12,8 +12,11 @@ struct _Matrix {
 typedef struct _Matrix *Matrix;
 
 extern Matrix matrix_new(size_t n_rows, size_t n_cols);
+extern Matrix matrix_new_like(Matrix m);
 extern int    matrix_free(Matrix m);
 extern int    matrix_set(Matrix m, size_t i, size_t j, double value);
 extern int    matrix_get(Matrix m, size_t i, size_t j, double *value);
+extern int    matrix_row_mult_const(Matrix m, size_t i, double c);
+extern int    matrix_row_add_row(Matrix m, size_t i1, size_t i2, double c);
 
 #endif
--- src/matrix.c
+++ src/matrix.c
@@ -2,7 +2,7 @@
 
 #include "matrix.h"
 
-#define INDEX(n_cols, row, col) (row - 1) * n_cols + (col - 1)
+#define MAT_INDEX(n_cols, row, col) (row - 1) * n_cols + (col - 1)
 
 Matrix
 matrix_new(size_t n_rows, size_t n_cols) {
@@ -19,6 +19,16 @@ matrix_new(size_t n_rows, size_t n_cols) {
   m->values = malloc(sizeof(double) * n_rows * n_cols);
 
   return m;
+}
+
+Matrix
+matrix_new_like(Matrix m) {
+
+  if (!m)
+    return NULL;
+
+  Matrix like_m = matrix_new(m->n_rows, m->n_cols);
+  return like_m;
 }
 
 int
@@ -45,7 +55,7 @@ matrix_set(Matrix m, size_t i, size_t j, double value) {
   if (j < 1 || j > m->n_cols)
     return -1;
 
-  m->values[INDEX(m->n_cols, i, j)] = value;
+  m->values[MAT_INDEX(m->n_cols, i, j)] = value;
 
   return 0;
 }
@@ -60,7 +70,47 @@ matrix_get(Matrix m, size_t i, size_t j, double *value) {
   if (j == 0 || j > m->n_cols)
     return -1;
 
-  *value = m->values[INDEX(m->n_cols, i, j)];
+  *value = m->values[MAT_INDEX(m->n_cols, i, j)];
+
+  return 0;
+}
+
+int
+matrix_row_mult_const(Matrix m, size_t i, double c) {
+
+  if (!m)
+    return -1;
+  if (i == 0 || i > m->n_rows)
+    return -1;
+
+  size_t n_cols = m->n_cols;
+  double val;
+
+  for (size_t j = 1; j <= n_cols; j++) {
+    val = m->values[MAT_INDEX(n_cols, i, j)];
+    m->values[MAT_INDEX(n_cols, i, j)] *= c * val;
+  }
+
+  return 0;
+}
+
+int
+matrix_row_add_row(Matrix m, size_t i1, size_t i2, double c) {
+
+  if (!m)
+    return -1;
+  if (i1 == 0 || i1 > m->n_rows)
+    return -1;
+  if (i2 == 0 || i2 > m->n_rows)
+    return -1;
+
+  size_t n_cols = m->n_cols;
+  double val;
+
+  for (size_t j = 1; j <= n_cols; j++) {
+    val = m->values[MAT_INDEX(n_cols, i2, j)];
+    m->values[MAT_INDEX(n_cols, i1, j)] *= c * val;
+  }
 
   return 0;
 }
