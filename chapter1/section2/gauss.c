334d53505d2f15a070ae435cf3e17d898f42d37f
Marian Domanski <mmdski@gmail.com>
2022-06-14 23:26:29 -0500

    add problem 1.2.1

--- chapter1/section2/gauss.c
+++ chapter1/section2/gauss.c
@@ -1,8 +1,10 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "gauss.h"
 
-Matrix
-gauss_solve(Matrix a, Matrix b) {
-
+int
+gauss_init_augmented(Matrix a, Matrix b, Matrix *aug_ptr) {
   if (!a)
     return -1;
   if (!b)
@@ -17,21 +19,155 @@ gauss_solve(Matrix a, Matrix b) {
 
   size_t n_rows     = a->n_rows;
   size_t n_cols_a   = a->n_cols;
-  size_t n_cols_b   = b->n_cols;
   size_t n_cols_aug = a->n_cols + b->n_cols;
 
-  Matrix augmented = matrix_new(n_rows, n_cols_aug);
+  Matrix aug      = *aug_ptr;
+  bool   free_aug = false;
+  if (!aug) {
+    aug      = matrix_new(n_rows, n_cols_aug);
+    *aug_ptr = aug;
+    free_aug = true;
+  } else {
+    if (aug->n_rows != n_rows || aug->n_cols != n_cols_aug)
+      return -1;
+  }
 
-  double value;
+  if (matrix_fill(aug, a, 1, 1) < 0)
+    goto fail;
+  if (matrix_fill(aug, b, 1, n_cols_a + 1) < 0)
+    goto fail;
 
-  for (size_t i = 1; i <= n_rows; i++) {
-    for (size_t j = 1; j <= n_cols_a; j++) {
-      if (matrix_get(a, i, j, &value) < 0)
+  return 0;
+
+fail:
+  if (free_aug) {
+    matrix_free(aug);
+    *aug_ptr = NULL;
+  }
+  return -1;
+}
+
+int
+gauss_reduce(Matrix aug) {
+
+  if (!aug)
+    return -1;
+
+  size_t n_rows = aug->n_rows;
+  size_t n_cols = aug->n_cols;
+
+  if (n_cols <= n_rows)
+    return -1;
+
+  double multiplier;
+  double pivot_value;
+  double row_value;
+  size_t pivot_row = 1;
+  size_t n_cols_a  = n_rows;
+
+  for (size_t j = 1; j <= n_cols_a; j++) {
+
+    if (matrix_get(aug, pivot_row, j, &pivot_value) < 0)
+      return -1;
+
+    for (size_t i = pivot_row + 1; i <= n_rows; i++) {
+
+      if (matrix_get(aug, i, j, &row_value) < 0)
         return -1;
-      if (matrix_set(augmented, i, j, value) < 0)
+
+      multiplier = -row_value / pivot_value;
+
+      if (matrix_add_row(aug, i, pivot_row, multiplier) < 0)
+        return -1;
+
+      if (matrix_set(aug, i, j, 0) < 0)
         return -1;
     }
+
+    pivot_row++;
   }
 
   return 0;
+}
+
+int
+gauss_back_sub(Matrix g_reduced, Matrix *x_ptr) {
+
+  if (!g_reduced)
+    return -1;
+
+  size_t n_rows   = g_reduced->n_rows;
+  size_t n_cols_g = g_reduced->n_cols;
+
+  // the last column is b
+  size_t n_cols_a = n_cols_g - 1;
+
+  // a in the reduced matrix must be square
+  if (n_rows != n_cols_a)
+    return -1;
+
+  Matrix x      = *x_ptr;
+  bool   free_x = false;
+  if (!x) {
+    x      = matrix_new(n_rows, 1);
+    *x_ptr = x;
+    free_x = true;
+  } else {
+    if ((x->n_rows != n_rows) || (x->n_cols != 1))
+      goto fail;
+  }
+
+  double  c_value;
+  double  t_value;
+  double  x_sum;
+  double *x_values = x->values;
+
+  for (size_t i = n_rows; i > 0; i--) {
+    if ((matrix_get(g_reduced, i, n_rows + 1, &c_value)) < 0)
+      goto fail;
+
+    x_sum = c_value;
+
+    for (size_t j = i + 1; j <= n_cols_a; j++) {
+
+      if ((matrix_get(g_reduced, i, j, &t_value)) < 0)
+        goto fail;
+
+      x_sum -= t_value * x_values[i - 1];
+    }
+
+    if ((matrix_get(g_reduced, i, i, &t_value)) < 0)
+      goto fail;
+
+    x_values[i - 1] = x_sum / t_value;
+  }
+
+  return 0;
+
+fail:
+  if (free_x) {
+    matrix_free(x);
+    *x_ptr = NULL;
+  }
+  return -1;
+}
+
+int
+gauss_solve(Matrix a, Matrix b, Matrix *x_ptr) {
+
+  Matrix aug = NULL;
+  if (gauss_init_augmented(a, b, &aug) < 0)
+    goto fail;
+
+  if (gauss_reduce(aug) < 0)
+    goto fail;
+
+  if (gauss_back_sub(aug, x_ptr) < 0)
+    goto fail;
+
+  return 0;
+
+fail:
+  matrix_free(aug);
+  return -1;
 }
---
+++ chapter1/section2/problem1.2.1.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <matrix.h>
+
+#include "gauss.h"
+
+int
+main(void) {
+
+  double vals[] = {1, 1, 1, 1, 2, 2, 1, 2, 3};
+  size_t n_rows = 3;
+  size_t n_cols = 3;
+
+  Matrix a = matrix_from(n_rows, n_cols, vals);
+  Matrix b = matrix_ones(n_rows, 1);
+  Matrix x = NULL;
+
+  puts("Ax=b");
+
+  puts("A:");
+  matrix_printf(a);
+
+  puts("\nb:");
+  matrix_printf(b);
+
+  if (gauss_solve(a, b, &x) < 0)
+    return EXIT_FAILURE;
+
+  puts("\nx:");
+  matrix_printf(x);
+
+  matrix_free(a);
+  matrix_free(b);
+  matrix_free(x);
+
+  return EXIT_SUCCESS;
+}
