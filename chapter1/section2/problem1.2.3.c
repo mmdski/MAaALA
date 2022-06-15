915c02ce12da4feca3d62fa5d738ffafa3bfb462
Marian Domanski <mmdski@gmail.com>
2022-06-15 07:44:50 -0500

    add problem 1.2.3

---
+++ chapter1/section2/problem1.2.3.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <matrix.h>
+
+#include "gauss.h"
+
+int
+main(void) {
+
+  // manually swap rows 2 and 1
+  double x_vals[] = {-1, 7, -5, 0, 4, -3, -1, 8, -6};
+  double b_vals[] = {4, 3, 5};
+  size_t n_rows   = 3;
+  size_t n_cols   = 3;
+
+  Matrix a = matrix_from(n_rows, n_cols, x_vals);
+  Matrix b = matrix_from(n_rows, 1, b_vals);
+  Matrix x = NULL;
+
+  puts("Ax=b");
+
+  puts("A:");
+  matrix_printf(a);
+
+  puts("\nb:");
+  matrix_printf(b);
+
+  if (gauss_solve(a, b, &x) < 0)
+    return EXIT_FAILURE;
+
+  puts("\nx:");
+  matrix_printf(x);
+
+  return EXIT_SUCCESS;
+}
