238880deb9c353b31e9cfbec33f87292c580d0f3
Marian Domanski <mmdski@gmail.com>
2022-06-15 16:37:17 -0500

    add problems 1.2.8 and 1.2.9

---
+++ chapter1/section2/problem1.2.8.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <matrix.h>
+
+#include "gauss.h"
+
+int
+main(void) {
+
+  double x_vals[] = {-1, 3, -2, -1, 4, -3, -1, 5, -4};
+  double b_vals[] = {1, 0, 0};
+  size_t n_rows   = 3;
+  size_t n_cols   = 3;
+
+  Matrix a   = matrix_from(n_rows, n_cols, x_vals);
+  Matrix b   = matrix_from(n_rows, 1, b_vals);
+  Matrix aug = NULL;
+  gauss_init_augmented(a, b, &aug);
+  gauss_reduce(aug);
+
+  puts("Ax=b");
+
+  puts("A:");
+  matrix_printf(a);
+
+  puts("\nb:");
+  matrix_printf(b);
+
+  puts("\nReduced augmented matrix:");
+  matrix_printf(aug);
+
+  return EXIT_SUCCESS;
+}
