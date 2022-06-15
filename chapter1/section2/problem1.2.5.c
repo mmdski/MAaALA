66218daa090c0d93172001dc3136b5281aca2f1d
Marian Domanski <mmdski@gmail.com>
2022-06-15 15:22:27 -0500

    add problem 1.2.5

---
+++ chapter1/section2/problem1.2.5.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <matrix.h>
+
+#include "gauss.h"
+
+int
+main(void) {
+
+  double a_vals[] = {4, -8, 5, 4, -7, 4, 3, -4, 2};
+  size_t n_rows   = 3;
+  size_t n_cols   = 3;
+
+  Matrix a = matrix_from(n_rows, n_cols, a_vals);
+  Matrix b = matrix_eye(n_rows);
+  Matrix x = NULL;
+
+  puts("AX=B");
+
+  puts("A:");
+  matrix_printf(a);
+
+  puts("\nB:");
+  matrix_printf(b);
+
+  if (gauss_solve(a, b, &x) < 0)
+    return EXIT_FAILURE;
+
+  puts("\nX:");
+  matrix_printf(x);
+
+  return EXIT_SUCCESS;
+}
